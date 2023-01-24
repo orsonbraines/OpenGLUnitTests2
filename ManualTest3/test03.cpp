4e0ffba45d66b9756ae32f771266a7bbd2daf320
Orson Baines <obraines@gmail.com>
2023-01-24 16:46:19 -0500

    small chagne to unit test 3

--- ManualTest3/test03.cpp
+++ ManualTest3/test03.cpp
@@ -31,7 +31,7 @@ MessageCallback(GLenum source,
 		type, severity, message);
 }
 
-#define DEBUG_OPENGL(x) { x; GLenum error = glGetError(); if (error) { std::cerr << "Error on line " << __LINE__ << " : " << (int) error << std::endl; } }
+#define DEBUG_OPENGL(x) { x; GLenum error = glGetError(); if (error) { std::cerr << "Error on line " << __LINE__ << " : " << (int) error << std::endl; exit(1); } }
 
 
 int main( void )
@@ -109,7 +109,7 @@ int main( void )
 	GLuint textures[3];
 	DEBUG_OPENGL(glGenTextures(3, textures));
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 2; i++) {
 		DEBUG_OPENGL(glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, textures[i]));
 		GLenum internalFormat;
 		GLenum attachment;
@@ -118,11 +118,11 @@ int main( void )
 			internalFormat = GL_RGBA8;
 			attachment = GL_COLOR_ATTACHMENT0;
 			break;
+		//case 1:
+		//	internalFormat = GL_DEPTH_COMPONENT32F;
+		//	attachment = GL_DEPTH_ATTACHMENT;
+		//	break;
 		case 1:
-			internalFormat = GL_DEPTH_COMPONENT32F;
-			attachment = GL_DEPTH_ATTACHMENT;
-			break;
-		case 2:
 			internalFormat = GL_STENCIL_INDEX8;
 			attachment = GL_STENCIL_ATTACHMENT;
 			break;
@@ -136,45 +136,48 @@ int main( void )
 	DEBUG_OPENGL(GLenum check = glCheckFramebufferStatus(GL_FRAMEBUFFER); std::cerr << "GL check: " << check << std::endl; assert(check == GL_FRAMEBUFFER_COMPLETE););
 
 	do{
-		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+		DEBUG_OPENGL(glBindFramebuffer(GL_FRAMEBUFFER, framebuffer));
 
 		// Clear the screen
-		glClear( GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+		DEBUG_OPENGL(glClear( GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ));
 
 		// Use our shader
-		glUseProgram(programID);
+		DEBUG_OPENGL(glUseProgram(programID));
 
 		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
+		DEBUG_OPENGL(glEnableVertexAttribArray(0));
+		DEBUG_OPENGL(glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer));
+		DEBUG_OPENGL(glVertexAttribPointer(
 			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
 			3,                  // size
 			GL_FLOAT,           // type
 			GL_FALSE,           // normalized?
 			0,                  // stride
 			(void*)0            // array buffer offset
-		);
+		));
 
-		glEnable(GL_SAMPLE_MASK);
-		glEnable(GL_DEPTH_TEST);
-		glDepthFunc(GL_ALWAYS);
+		DEBUG_OPENGL(glEnable(GL_SAMPLE_MASK));
+		DEBUG_OPENGL(glEnable(GL_DEPTH_TEST));
+		DEBUG_OPENGL(glDepthFunc(GL_ALWAYS));
+		DEBUG_OPENGL(glEnable(GL_STENCIL_TEST));
+		DEBUG_OPENGL(glStencilFunc(GL_ALWAYS, 0, 0xff));
+		DEBUG_OPENGL(glStencilOp(GL_INCR, GL_INCR, GL_INCR));
 		
 		for (uint32_t i = 0; i < 4; ++i) {
 			// Draw the triangle !
 			for (int j = 0; j < 3; ++j) {
 				g_vertex_buffer_data[3 * j + 2] = 0.3f * i;
 			}
-			glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(g_vertex_buffer_data), g_vertex_buffer_data);
-			glUniform3f(colourLoc, 0.3f * i, 0.3f * i , 0.3f * i);
-			glSampleMaski(0, 1u  << i );
-			glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+			DEBUG_OPENGL(glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(g_vertex_buffer_data), g_vertex_buffer_data));
+			DEBUG_OPENGL(glUniform3f(colourLoc, 0.3f * i, 0.3f * i , 0.3f * i));
+			DEBUG_OPENGL(glSampleMaski(0, 1u  << i ));
+			DEBUG_OPENGL(glDrawArrays(GL_TRIANGLES, 0, 3)); // 3 indices starting at 0 -> 1 triangle
 		}
 
-		glDisableVertexAttribArray(0);
+		DEBUG_OPENGL(glDisableVertexAttribArray(0));
 
-		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-		glBlitFramebuffer(0, 0, 1024, 768, 0, 0, 1024, 768, GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT | GL_DEPTH_BUFFER_BIT, GL_NEAREST);
+		DEBUG_OPENGL(glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0));
+		DEBUG_OPENGL(glBlitFramebuffer(0, 0, 1024, 768, 0, 0, 1024, 768, GL_COLOR_BUFFER_BIT /* | GL_STENCIL_BUFFER_BIT | GL_DEPTH_BUFFER_BIT*/, GL_NEAREST));
 
 		// Swap buffers
 		glfwSwapBuffers(window);
